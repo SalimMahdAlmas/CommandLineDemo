ad377695cc10121b9f77734865ee986055182469
Sahid S Almas <robinaf3@gmail.com>
2015-10-10 18:37:17 +0530

    Create Command.java

---
+++ Command.java
@@ -0,0 +1,50 @@
+public abstract class Command {
+
+    /**
+     * To Store function names
+     */
+    private String mFunctionName;
+
+    /**
+     * To Store Argument
+     */
+    private String mArgument;
+
+    /**
+     * Initialing the class
+     * @param functionName The function name you want
+     * @param argument We push the argument to here
+     */
+    protected Command(String functionName,String argument) {
+        this.mArgument = argument;
+        this.mFunctionName = functionName;
+    }
+
+    /**
+     * If the command executed and with argument
+     *
+     *
+     *
+     * @param argument The argument comes to the method
+     */
+    public abstract void function(String argument);
+
+    /**
+     *
+     * @return Function Name we will use
+     */
+    public String getFunctionName() {
+        return mFunctionName;
+    }
+
+    /**
+     *
+     * @return Argument that will be returned here
+     */
+    public String getArgument() {
+        return mArgument;
+    }
+
+
+
+}
